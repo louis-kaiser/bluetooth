8f70dba8c285ab59862a2446cc8f23ce89959786
Louis Kaiser <louis.kaiser0203@icloud.com>
2024-05-02 12:12:10 +0200

    Initial Commit

---
+++ bluetooth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bluetooth
+//
+//  Created by Louis Kaiser on 2024-05-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bluetooth/bluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  bluetoothApp.swift
+//  bluetooth
+//
+//  Created by Louis Kaiser on 2024-05-02.
+//
+
+import SwiftUI
+
+@main
+struct bluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
